6d5a81083fa88e4b107faa45956ffe47430d84ab
Roothex200 <sabiturrahman60@gmail.com>
2025-06-09 10:54:21 +0600

    install firebase store and auth dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,25 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled1/firebase_options.dart';
 import 'package:untitled1/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Student Online Class',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:   SplashScreen( ),
+      home: const SplashScreen(),
     );
   }
 }
